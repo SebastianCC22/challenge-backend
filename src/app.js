3c22a38eddde05a8750b5e8812382fceb2e3648e
Sebastian <SebasTian.C.C1418@gmail.com>
2022-05-26 20:46:37 -0500

    Agrego Middleware, Uso morgan y las rutas

--- src/app.js
+++ src/app.js
@@ -1,5 +1,14 @@
 import express from "express";
+import morgan from "morgan";
+import personajesRoutes from './routes/personaje.routes.js';
 
 const app = express();
+
+app.use(morgan("dev"))
+
+//middlewares
+app.use(express.json())
+
+app.use(personajesRoutes)
 
 export default app;
